bc1ad43f6a32fc0592795b3d38c33832f4b4a346
seanjungmsba <seanjeong1996@gmail.com>
2022-08-10 11:55:16 -0400

    Implemented loggers

---
+++ src/main/java/com/example/logger1/SpringLogger1Application.java
@@ -0,0 +1,13 @@
+package com.example.logger1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogger1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLogger1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/logger1/SpringLogger1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logger1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogger1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
